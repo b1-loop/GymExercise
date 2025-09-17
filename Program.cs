4506c6c85df0e3c98faed015d46b2afd1b0bed27
b1-loop <bojidarivanov98@gmail.com>
2025-09-17 20:06:45 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GymExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
