12045cfa5441a05256150ac45a7bd945c734cd40
b1-loop <bojidarivanov98@gmail.com>
2025-09-20 12:32:45 +0200

    gjorde en abstrackt klass av user där jag tilldelade vilka pass tränarna har och hur vill kunderna betala

---
+++ Användare.cs
@@ -0,0 +1,12 @@
+﻿
+namespace GymExercise
+{
+    //skapar en abstrakt klass som heter User där andra klasser kan ärva från
+    public abstract class User
+    {
+        public string Name;
+        public string Surname;
+        public int Age;
+        public int Id;
+    }
+}
---
+++ Brukare.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymExercise
+{
+    public class Customer : User
+    {
+      public void HowToPay1()
+        {
+            Console.WriteLine("Denna kund vill betala månadsvist");
+        }
+
+        public void HowToPay2()
+            {
+                Console.WriteLine("Denna kund vill betala årsvist");
+        }
+    }
+}
